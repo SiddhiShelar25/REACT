60e91d86e8a8cc268b27cc6d09bf526434ccaefa
Siddhi Shelar <siddhishelar.sknsits.comp@gmail.com>
2024-12-08 00:52:38 +0530

    UseState hook

--- COUNTER_PROJECT/01_Counter/src/App.jsx
+++ COUNTER_PROJECT/01_Counter/src/App.jsx
@@ -1,37 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [counter, setCounter]  = useState(0)
+  const [counter, setCounter] = useState(0);
 
   //let counter = 15
 
   const addValue = () => {
+    if (counter >= 20) {
+      
+      return;
+    }
     //counter = counter + 1
-    setCounter(counter => counter + 1)
-    
-  }
+    setCounter((counter) => counter + 1);
+  };
 
   const removeValue = () => {
-    setCounter(counter - 1)
-  }
-  
+    if(counter <=0) return;
+    setCounter(counter - 1);
+  };
+
   return (
     <>
       <h1>Chai aur react</h1>
       <h2>Counter value: {counter}</h2>
 
-      <button
-      onClick={addValue}
-      >Add value </button> 
+      <button onClick={addValue}>Add value </button>
       <br />
-      <button
-      onClick={removeValue}
-      >remove value </button>
+      <button onClick={removeValue}>remove value </button>
       <p>footer: {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
